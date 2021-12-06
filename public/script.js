6bd947be9217e1b93ad1add0fcdd101fc002b2c7
amal roshan <44673099+amalazimroshan@users.noreply.github.com>
2021-12-06 13:16:28 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -13,26 +13,7 @@ function initMap() {
       map.setCenter(pos);
     });
   }
-  // marker = new google.maps.Marker({
-  //   position: new google.maps.LatLng(-34.397, 150.644),
-  //   map: map,
-  // });
-  // const userMarkerIcon =
-  //   '<svg style="width:54px;height:54px" viewBox="0 0 24 24">\
-  //       <path fill="hsl(59, 80%, 52%)"\
-  //           d="M12,2C15.31,2 18,4.66 18,7.95C18,12.41 12,19 12,19C12,19 6,12.41 6,7.95C6,4.66 8.69,2 12,2M12,6A2,2 0 0,0 10,8A2,2 0 0,0 12,10A2,2 0 0,0 14,8A2,2 0 0,0 12,6M20,19C20,21.21 16.42,23 12,23C7.58,23 4,21.21 4,19C4,17.71 5.22,16.56 7.11,15.83L7.75,16.74C6.67,17.19 6,17.81 6,18.5C6,19.88 8.69,21 12,21C15.31,21 18,19.88 18,18.5C18,17.81 17.33,17.19 16.25,16.74L16.89,15.83C18.78,16.56 20,17.71 20,19Z" />\
-  //   </svg>';
-  // const sharerMarkerImg = {
-  //   // url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-  //   url: "https://img.icons8.com/ios-filled/50/fa314a/marker.png",
-  //   scaledSize: new google.maps.Size(25, 25),
-  // };
-  // const userMarkerImg = {
-  //   url: "https://img.icons8.com/ios-filled/50/fa314a/place-marker--v1.png",
-  //   scaledSize: new google.maps.Size(25, 25),
-  //   /*    origin: new google.maps.Point(0, 0),
-  //     anchor: new google.maps.Point(0, 32), */
-  // };
+  
   var UserIcon = {
     path: "M12,2C15.31,2 18,4.66 18,7.95C18,12.41 12,19 12,19C12,19 6,12.41 6,7.95C6,4.66 8.69,2 12,2M12,6A2,2 0 0,0 10,8A2,2 0 0,0 12,10A2,2 0 0,0 14,8A2,2 0 0,0 12,6M20,19C20,21.21 16.42,23 12,23C7.58,23 4,21.21 4,19C4,17.71 5.22,16.56 7.11,15.83L7.75,16.74C6.67,17.19 6,17.81 6,18.5C6,19.88 8.69,21 12,21C15.31,21 18,19.88 18,18.5C18,17.81 17.33,17.19 16.25,16.74L16.89,15.83C18.78,16.56 20,17.71 20,19Z",
     fillColor: "#f48c06",
